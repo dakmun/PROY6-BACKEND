c4dd5079aa7f86f86d61f1269d300cd3543575d2
Marcelo M. <mmundaca.27@outlook.cl>
2024-08-28 23:38:17 -0400

    Añadir logs para depuración en authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  // Leer el encabezado Authorization
+  // Agregar log para verificar el valor del encabezado Authorization
   const token = req.header('Authorization');
+  console.log('Authorization Header:', token);  // <-- Agrega este log
 
-  // Verificar si el token está presente
   if (!token) {
     return res.status(401).json({ msg: 'No token, autorización denegada' });
   }
 
   try {
-    // Eliminar "Bearer " de la cadena del token
+    // Verifica si el token tiene la estructura correcta y se puede decodificar
     const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
-    req.user = decoded.id; // Establecer el ID del usuario en la solicitud
+    console.log('Decoded Token:', decoded);  // <-- Agrega este log
+    req.user = decoded.id;
     next();
   } catch (err) {
+    console.log('JWT Error:', err.message);  // <-- Agrega este log para ver cualquier error relacionado con JWT
     res.status(401).json({ msg: 'Token no válido' });
   }
 };
