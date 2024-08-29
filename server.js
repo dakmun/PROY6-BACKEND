dc9ae26cb13b9ced56fec230b23a9d7405bd9ea8
Marcelo M. <mmundaca.27@outlook.cl>
2024-08-28 20:00:36 -0400

    Update server to use process.env.PORT

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Configuración del puerto
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
